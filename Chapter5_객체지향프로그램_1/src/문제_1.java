4913f089159a86d3e9bb1b7b7df8dd59a18ce59a
SIST <SIST@DESKTOP-3C0I428>
2023-04-12 17:52:34 +0900

    1111

---
+++ Chapter5_객체지향프로그램_1/src/문제_1.java
@@ -0,0 +1,89 @@
+class Movie
+{
+	String poster;
+	String title;
+	String subTitle;
+	String movieEn;
+	String type;
+	double score;
+	int boxoffice;
+	String country;
+	String age;
+	String time;
+	String story;
+}
+class News
+{
+	String poster;
+	int no;
+	String title;
+	String sysnopsis;
+	int time;
+	String compony;
+}
+class 만개의_레시피
+{
+	String poster;
+	int no;
+	String title;
+	String subject;
+}
+class 미라이
+{
+	String name;
+	double score;
+	String address;
+	int tel;
+	String type;
+	String price;
+	String parking;
+	String stime,etime;
+	String[] menu;
+}
+class Recipe
+{
+	String poster;
+	int no;
+	String title;
+	String userName;
+	String userImage;
+	String score;
+	int hit;
+}
+class 멜론
+{
+	String poster;
+	int no;
+	String state;
+	String pic;
+	String music;
+	String singer;
+	String album;
+	int like;
+}
+class Food
+{
+	String poster;
+	int no;
+	String name;
+	int sale;
+	int price;
+}
+class Play
+{
+	String poster;
+	String name;
+	boolean state;
+	String speriod,eperiod;
+	String time;
+	String place;
+	String member;
+	String age;
+	String playtime;
+}
+public class 문제_1 {
+
+	public static void main(String[] args) {
+		
+	}
+}
